ec8c1fe58fd04844aea3e9024eafb1eafef065b1
Marcus Veloso <desenvolvedoroficial@gmail.com>
2021-11-02 08:09:13 -0300

    update module references

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
+import { MenuComponent } from './navegacao/menu/menu.component';
+import { NotFoundComponent } from './navegacao/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+    HomeComponent,
+    FooterComponent,
+    MenuComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
